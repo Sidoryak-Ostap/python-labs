7562062629622555826806ca4045c0304018a069
Sidoryak-Ostap <ostapsidorak@gmail.com>
2023-10-19 22:00:55 +0300

    [lab 5 changes final v.]

--- lab5/file.py
+++ lab5/file.py
@@ -20,18 +20,22 @@ class DataBaseActions():
     _file_instance = None
     def __init__(self, file_path):
         self._file_path = file_path
-        self.inititialize_file()
+        # self.inititialize_file()
+
+    def open_file(self,path,  mode='r'):
+        file = open(path, mode, encoding='utf-8', newline='')
+        return file
 
 # Ініцілізувати файл з даними
     def inititialize_file(self):
         self.get_file_instance('w')
         
-        writer = csv.writer(self._file_instance, delimiter=',', )
+        writer = csv.writer(self._file_instance, delimiter=',' )
         writer.writerows(car_list)
             
     def get_file_instance(self, mode):
         if(self._file_instance != None): self._file_instance.close()
-        self._file_instance = Singleton().open_file(self._file_path, mode)
+        self._file_instance = self.open_file(self._file_path, mode)
     
 # Читання файлу
     def read_info(self):
@@ -49,18 +53,17 @@ class DataBaseActions():
         if(info !=None and type(info) is type(list())):
             writer.writerow(info)
         else:
-            list = []
             print("Введіть марку, модель, рік та колір автомобіля")
+            list = []
             for i in range(4):
                 list.append(input())
             writer.writerow(list)
 # Пошук за фільтром
     def search_info(self):
         self.get_file_instance('r')
-        csv_reader = csv.reader(self._file_instance)
-        list = []
-        for line in csv_reader:
-            list.append(line)
+        
+        list = self.read_data()
+        
             
         search_by = int(input("Виберіть фільтр для пошуку інормації\n1. Марка\n2. Модель\n3. Рік випуску\n4. Колір\n"))
         search_value = input("Введіть значеня: ")
@@ -74,10 +77,7 @@ class DataBaseActions():
         self.get_file_instance('r')
         car_number = int(input('Введіть номер запису який ви хочете видалити:  '))
         
-        data = []
-        csv_reader= csv.reader(self._file_instance)
-        for item in csv_reader:
-            data.append(item)
+        data = self.read_data()
         
         
         del data[car_number]
@@ -91,10 +91,7 @@ class DataBaseActions():
         update_by = int(input("Виберіть що ви хочете поміняти\n1. Марка\n2. Модель\n3. Рік випуску\n4. Колір\n"))
         update_value = input("Введіть значення: ")
         
-        data = []
-        csv_reader= csv.reader(self._file_instance)
-        for item in csv_reader:
-            data.append(item)
+        data = self.read_data()
         
         data[car_number][update_by - 1] = update_value
         self.insert_new_data(data)
@@ -102,10 +99,8 @@ class DataBaseActions():
 # Сортування інформації
     def sort_info(self):
         self.get_file_instance('r')
-        data = []
-        csv_reader= csv.reader(self._file_instance)
-        for item in csv_reader:
-            data.append(item)
+        data = self.read_data()
+        
         del data[0]
         sort_by = int(input("Виберіть фільтр для сортування інормації\n1. Марка\n2. Модель\n3. Рік випуску\n4. Колір\n"))
         
@@ -120,7 +115,13 @@ class DataBaseActions():
         self.get_file_instance('w')
         csv_writer = csv.writer(self._file_instance)
         csv_writer.writerows(data)
-        
+    
+    def read_data(self):
+        data = []
+        csv_reader= csv.reader(self._file_instance)
+        for item in csv_reader:
+            data.append(item)
+        return data
             
 # Виведення середнього року випуску кожної марки
     def average_year_of_car_mark(self):
@@ -154,18 +155,17 @@ class DataBaseActions():
         for i in new_list:
             print(i[0][0],"\t", round((int(i[0][1]) / i[1][1])))
 
-class Singleton():
+
+
+class Singleton(DataBaseActions):
     _instance = None
 
-    def __new__(cls):
+    def __new__(cls, path):
+        print(path)
         if not cls._instance:
             cls._instance = super().__new__(cls)
         return cls._instance
 
-
-    def open_file(self, path, mode='r'):
-        file = open(path, mode, encoding='utf-8', newline='')
-        return file
     
     
     
@@ -176,5 +176,5 @@ class Singleton():
         
     
     
-database= DataBaseActions('test.csv')
-database.average_year_of_car_mark()
+database= Singleton('test.csv')
+print(database)
